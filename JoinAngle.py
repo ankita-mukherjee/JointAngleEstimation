9a4a8d08988bb22c1437cd0cefd378158602dc92
MIC-Lab <mic.lab.edu@gmail.com>
2022-04-07 16:48:46 -0700

    4-6

--- JoinAngle.py
+++ JoinAngle.py
@@ -215,19 +215,6 @@ if "movenet_thunder" in model_name:
 else:
     raise ValueError("Unsupported model name: %s" % model_name)
 
-def movenet(input_image):
-
-    model = module.signatures['serving_default']
-
-    # SavedModel format expects tensor type of int32.
-    input_image = tf.cast(input_image, dtype=tf.int32)
-    # Run model inference.
-    outputs = model(input_image)
-    # Output is a [1, 1, 17, 3] tensor.
-    keypoints_with_scores = outputs['output_0'].numpy()
-    return keypoints_with_scores
-
-    raise ValueError("Unsupported model name: %s" % model_name)
 
 def movenet(input_image):
 
