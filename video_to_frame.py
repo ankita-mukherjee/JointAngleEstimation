e67944453520c48a997b50ab940111b12fc6012f
MIC-Lab <mic.lab.edu@gmail.com>
2022-04-08 14:06:32 -0700

    angelFunciton

---
+++ video_to_frame.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+path = "./dl009/elbflex/"
+dir_list = os.listdir(path)
+video_files = list(filter(lambda x: '.avi' in x, dir_list)) # Keep all video files
+print('Video Files \n:', video_files)
+
+for video_name in video_files:
+
+    print ('Cheking', video_name)
+    isFile = os.path.isdir(path+"Frames-"+video_name[:-4])
+
+    if not isFile:
+        print('No Frames. Making Dir: \n', path+"Frames-"+video_name[:-4])
+        os.mkdir(path+"Frames-"+video_name[:-4])
+        print('Processing...')
+        vidcap = cv2.VideoCapture(path + video_name)
+        success, image = vidcap.read() # Grabs, decodes and returns the next video frame
+        
+        count = 0
+        while success:
+            cv2.imwrite(path+"Frames-"+video_name[:-4]+"/%d.jpg" % count, image)     # save frame as JPEG file      
+            success, image = vidcap.read()
+            count += 1
+        print('Processing success', count, "frames saved")
+        
