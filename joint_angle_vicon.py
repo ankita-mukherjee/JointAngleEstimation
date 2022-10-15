fix get_joint_name in calculate_error.py and added shoulder and elbow angle calculation for vicon

joint_angle_vicon.py
@@ -124,12 +124,119 @@ def calculate_knee_angles(path_knee):
 
 # Calculate shoulder abduction, shoulder extension and shoulder flexion joint angles.
 def calculate_shoulder_angles(path_shoulder):
-    ...
+    all_files_shoulder = glob.glob(path_shoulder + "*.csv")
+    for trial_shoulder in all_files_shoulder:
+        df1 = pd.read_csv(
+            trial_shoulder,
+            skiprows=5,
+            header=None,
+            names=[
+                "Frame",
+                "SubFrame",
+                "ASI_X_mm",
+                "ASI_Y_mm",
+                "ASI_Z_mm",
+                "SHO_X_mm",
+                "SHO_Y_mm",
+                "SHO_Z_mm",
+                "ELB_X_mm",
+                "ELB_Y_mm",
+                "ELB_Z_mm",
+                "WRI_X_mm",
+                "WRI_Y_mm",
+                "WRI_Z_mm",
+            ],
+        )
+
+        shohip_length = np.sqrt(
+            (df1["SHO_X_mm"] - df1["ASI_X_mm"]) ** 2
+            + (df1["SHO_Y_mm"] - df1["ASI_Y_mm"]) ** 2
+            + (df1["SHO_Z_mm"] - df1["ASI_Z_mm"]) ** 2
+        )
+        shoelb_length = np.sqrt(
+            (df1["SHO_X_mm"] - df1["ELB_X_mm"]) ** 2
+            + (df1["SHO_Y_mm"] - df1["ELB_Y_mm"]) ** 2
+            + (df1["SHO_Z_mm"] - df1["ELB_Z_mm"]) ** 2
+        )
+        elbhip_length = np.sqrt(
+            (df1["ELB_X_mm"] - df1["ASI_X_mm"]) ** 2
+            + (df1["ELB_Y_mm"] - df1["ASI_Y_mm"]) ** 2
+            + (df1["ELB_Z_mm"] - df1["ASI_Z_mm"]) ** 2
+        )
+
+        cASIa = (shohip_length**2 + shoelb_length**2 - elbhip_length**2) / (
+            2 * shohip_length * shoelb_length
+        )
+        shoulder_angle_rad = np.arccos(cASIa)
+        theta_shoulder_angle = shoulder_angle_rad * 180 / math.pi
+        theta_shoulder_angle = pd.Series(
+            theta_shoulder_angle.tolist(), index=df1["Frame"].values
+        )
+        # Example: "./data/dl103-Right-Lower-obj1/hipabd/vicon/"
+        processed_folder_path = os.path.split(trial_shoulder)[0] + "/vicon/"
+        os.makedirs(processed_folder_path, exist_ok=True)
+        csv_file_path = processed_folder_path + os.path.split(trial_shoulder)[1]
+        theta_shoulder_angle.to_csv(csv_file_path, header=False)
+        print(f"Generated hip joint angles from vicon data to {csv_file_path}")
 
 
-# Calculate elbox flexion joint angle.
-def calculate_elbow_angles(path_elbow):
-    ...
+# Calculate elbow flexion joint angle.
+def calculate_elb_angles(path_elb):
+    all_files_elb = glob.glob(path_elb + "*.csv")
+
+    for trial_elbow in all_files_elb:
+        df1 = pd.read_csv(
+            trial_elbow,
+            skiprows=5,
+            header=None,
+            names=[
+                "Frame",
+                "SubFrame",
+                "ASI_X_mm",
+                "ASI_Y_mm",
+                "ASI_Z_mm",
+                "SHO_X_mm",
+                "SHO_Y_mm",
+                "SHO_Z_mm",
+                "ELB_X_mm",
+                "ELB_Y_mm",
+                "ELB_Z_mm",
+                "WRI_X_mm",
+                "WRI_Y_mm",
+                "WRI_Z_mm",
+            ],
+        )
+
+        shoelb_length = np.sqrt(
+            (df1["SHO_X_mm"] - df1["ELB_X_mm"]) ** 2
+            + (df1["SHO_Y_mm"] - df1["ELB_Y_mm"]) ** 2
+            + (df1["SHO_Z_mm"] - df1["ELB_Z_mm"]) ** 2
+        )
+        elbwri_length = np.sqrt(
+            (df1["ELB_X_mm"] - df1["WRI_X_mm"]) ** 2
+            + (df1["ELB_Y_mm"] - df1["WRI_Y_mm"]) ** 2
+            + (df1["ELB_Z_mm"] - df1["WRI_Z_mm"]) ** 2
+        )
+        showri_length = np.sqrt(
+            (df1["SHO_X_mm"] - df1["WRI_X_mm"]) ** 2
+            + (df1["SHO_Y_mm"] - df1["WRI_Y_mm"]) ** 2
+            + (df1["SHO_Z_mm"] - df1["WRI_Z_mm"]) ** 2
+        )
+
+        cELBa = (shoelb_length**2 + elbwri_length**2 - showri_length**2) / (
+            2 * shoelb_length * elbwri_length
+        )
+        elbow_angle_rad = np.arccos(cELBa)
+        theta_elbow_angle = elbow_angle_rad * 180 / math.pi
+        theta_elbow_angle = pd.Series(
+            theta_elbow_angle.tolist(), index=df1["Frame"].values
+        )
+        # Example: "./data/dl103-Right-Lower-obj1/hipabd/vicon/"
+        processed_folder_path = os.path.split(trial_elbow)[0] + "/vicon/"
+        os.makedirs(processed_folder_path, exist_ok=True)
+        csv_file_path = processed_folder_path + os.path.split(trial_elbow)[1]
+        theta_elbow_angle.to_csv(csv_file_path, header=False)
+        print(f"Generated hip joint angles from vicon data to {csv_file_path}")
 
 
 if __name__ == "__main__":
@@ -150,7 +257,7 @@ if __name__ == "__main__":
                 calculate_knee_angles(path_knee=movement_path)
             elif "sho" in movement.lower():
                 calculate_shoulder_angles(path_shoulder=movement_path)
-            elif "elbow" in movement.lower():
-                calculate_elbow_angles(path_elbow=movement_path)
+            elif "elb" in movement.lower():
+                calculate_elb_angles(path_elb=movement_path)
             else:
                 raise ValueError(f"unknown movement {movement}")